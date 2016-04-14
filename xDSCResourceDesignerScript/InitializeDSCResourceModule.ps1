9470e085ebf56a7ecf2839a5a49163cd76a8414d
Ben Gelens <ben@bgelens.nl>
2016-04-14 10:31:15 +0200

    Fixed construct bug in test, missing else statement. Added verbose messages. Increase version number. Added Pester Tests

--- xDSCResourceDesignerScript/InitializeDSCResourceModule.ps1
+++ xDSCResourceDesignerScript/InitializeDSCResourceModule.ps1
@@ -1,4 +1,4 @@
-﻿New-xDscResource -ModuleName HybridRunbookWorker -Name BG_HybridRunbookWorker -FriendlyName HybridRunbookWorker -Path C:\GIT\ -ClassVersion 1.0.0.0 -Property @(
+﻿New-xDscResource -ModuleName HybridRunbookWorker -Name MSFT_HybridRunbookWorker -FriendlyName HybridRunbookWorker -Path C:\GIT\ -ClassVersion 1.0.0.0 -Property @(
     New-xDscResourceProperty -Name Ensure -Type String -ValidateSet 'Present','Absent' -Attribute Required
     New-xDscResourceProperty -Name Endpoint -Type String -Attribute Key
     New-xDscResourceProperty -Name Token -Type PSCredential -Attribute Required
